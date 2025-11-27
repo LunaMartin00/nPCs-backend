3af17527aa783767f64b49886b40445486a66bdc
CarlosCruz99 <00013324@uca.edu.sv>
2025-11-26 22:11:25 -0600

    feat: Agregar controlador para añadir productos

--- router/router.js
+++ router/router.js
@@ -6,7 +6,7 @@ import { verifyToken } from "../utils/middleware/verifyToken.js";
 import { signIn } from "../controllers/signIn.js";
 import { signUpUser } from "../controllers/signUpUser.js";
 import { signUpStoreManager } from "../controllers/signUpStoreManager.js";
-// import { addProduct } from "../controllers/addProduct.js";
+import { addProducts } from "../controllers/addProducts.js";
 import { getProductsByCategory } from "../controllers/getProductsByCategory.js";
 
 const router = express.Router();
@@ -15,7 +15,7 @@ const router = express.Router();
 router.post("/signIn", signIn);
 router.post("/signUp/cliente", signUpUser);
 router.post("/signUp/tienda", signUpStoreManager);
-// router.post("/addProduct", addProduct)
+router.post("/addProduct", verifyToken, addProducts)
 router.get("/products/:category", getProductsByCategory);
 
 export default router;
